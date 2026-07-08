0310a12003cb8faf9d2ab087627c50712b351d89
JahidulIslam305 <jahidulislam66305@gmail.com>
2026-07-08 22:09:32 +0600

    Add files via upload

---
+++ students.js
@@ -0,0 +1,25 @@
+const students = [
+    {
+        id:1,
+        name:"Rahim",
+        department:"CSE",
+        cgpa:3.75,
+        isActive:true
+    },
+    {
+        id:2,
+        name:"Karim",
+        department:"EEE",
+        cgpa:3.20,
+        isActive:false
+    },
+    {
+        id:3,
+        name:"Sakib",
+        department:"BBA",
+        cgpa:3.90,
+        isActive:true
+    }
+]
+
+export default students;
